a8764983512abf07a60238181b8569cb3cc3eb31
abar <atbarlow@protonmail.com>
2025-01-25 01:32:48 -0700

    rename docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t movie-server .
-docker rm -f movie-server-container &>/dev/null
+docker build -t griffith .
+docker rm -f griffith-container &>/dev/null
 docker volume prune -f
-docker run -d -p 1935:1935 -p 8080:8080 --name movie-server-container movie-server
+docker run -d -p 1935:1935 -p 8080:8080 --name griffith-container griffith
